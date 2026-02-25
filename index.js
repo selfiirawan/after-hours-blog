9b08ad022b83c6a805af5b66080d6eb19f47570b
Selfi <celpi.irawan@gmail.com>
2026-02-26 00:16:42 +0800

    restore project after git corruption

--- index.js
+++ index.js
@@ -1,51 +1,68 @@
 import express from "express";
 import bodyParser from "body-parser";
+import slugify from "slugify";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const posts = [];
 
+// HOME  
 app.get("/", (req, res) => {
     res.render("index.ejs", { 
         posts: posts,
     });
 })
 
+// ABOUT
 app.get("/about", (req, res) => {
     res.render("about.ejs");
 })
 
+// CREATE 
 app.get("/create", (req, res) => {
     res.render("create.ejs");
 })
 
-app.get("/view/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find(p => p.id === id);
+// VIEW FULL POST 
+app.get("/view/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find(p => p.slug === slug);
 
     res.render("view.ejs", { post: post });
 })
 
-app.get("/edit/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts.find(p => p.id === id);
+app.get("/edit/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find(p => p.slug === slug);
 
     res.render("edit.ejs", { post: post });
 })
 
-app.post("/edit/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const post = posts[id-1];
+app.post("/edit/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find(p => p.slug === slug);
 
     if (post) {
         post.topic = req.body.topic;
         post.content = req.body.content;
+        post.slug = slugify(req.body.topic, { lower: true, strict: true });
     }
-    res.redirect(`/view/${id}`);
+    res.redirect(`/view/${post.slug}`);
+})
+
+app.post("/delete/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const index = posts.findIndex(p => p.slug === slug);
+
+    if (index !== -1) {
+        posts.splice(index, 1);
+    }
+    res.redirect("/");
 })
 
 app.post("/submit", (req, res) => {
@@ -53,9 +70,9 @@ app.post("/submit", (req, res) => {
     const content = req.body.content;
     
     const newPost = {
-        id: posts.length + 1, 
         topic: topic,
-        content: content
+        content: content, 
+        slug: slugify(topic, { lower: true, strict: true })
     }
 
     posts.push(newPost);
