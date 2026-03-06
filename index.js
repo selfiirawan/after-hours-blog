872ce52b39729e9f76d5152a0f04b41e832a63ea
Selfi <celpi.irawan@gmail.com>
2026-03-06 19:07:13 +0800

    prep for deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import slugify from "slugify";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
